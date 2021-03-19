c180abe3dd642266e09acc9294b6277794dda304
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-19 15:06:07 -0400

    created BookRow.js

---
+++ src/Components/BookRow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookRow extends React.Component {
+    render() {
+        return (
+            <div>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default BookRow
