cf355c733bc40d468f433720f15fdd1fa47de7b0
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-03-19 15:04:59 -0400

    working on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Title from './Components/Title';
+import { Search } from './Components/Search';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Search />
     </div>
   );
 }
